3dbfef4a7626599557d8583ee34025aab335d245
temuri pkhaladze <kverna133@gmail.com>
2016-01-04 04:22:44 +0400

    edit animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-$('.container > div').on('click', function (){
+$('.container > div.basic').on('click', function (){
+ scope = $(this);
+ if(!scope.hasClass("basic"))return '';
+ slide=scope.data("slide");
  topp=100*parseFloat($(this).css("top"))/parseFloat($("body").css("height"));
- $(".left").css("top",topp+"%").removeClass("one-bl two-bl thre-bl four-bl five-bl");
- $(this).removeClass("right").addClass('left').siblings('.transition').addClass('right').removeClass("left");
+ $(".active").css("top",topp+"%").addClass(slide+"active allin");
+ scope.removeClass("blocks allin")
+	.prependTo('.container').hide();
+ scope.show().addClass(slide+"active active").siblings()
+ 	.addClass(slide+'blocks blocks').removeClass("active");
+ $('.container > div.basic').removeClass("basic");
+});
+$(".container").on("click","[class*='blocks']",function(){
+	array=$(".active").attr("class").split(" ");
+	index=array.indexOf("active");
+	active=array[index-1];
+	fixactive=active.substr(0,active.length-6);
+	$(".active").addClass("allin "+fixactive+"blocks blocks").removeClass(fixactive+"active active");
+	scope=$(this);
+	scope.addClass(fixactive+"active active").removeClass(fixactive+"blocks blocks");
+	setTimeout(function(){scope.prependTo('.container');},50);
 });
