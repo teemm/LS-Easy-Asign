f50b0a36c9546655739a75ab192ae807f38784c7
teemm <kverna133@gmail.com>
2016-01-06 18:51:37 +0400

    without bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
-$('.container > div.basic').on('click', function (){
- scope = $(this);
- if(!scope.hasClass("basic"))return '';
- slide=scope.data("slide");
- topp=100*parseFloat($(this).css("top"))/parseFloat($("body").css("height"));
- $(".active").css("top",topp+"%").addClass(slide+"active allin");
- scope.removeClass("blocks allin")
+	
+	$('.container > div.basic').on('click', function (){
+ 	scope = $(this);
+ 	if(!scope.hasClass("basic"))return '';
+ 	slide=scope.data("slide");
+ 	topp=100*parseFloat($(this).css("top"))/parseFloat($("body").css("height"));
+ 	$(".active").css("top",topp+"%").addClass(slide+"active all");
+ 	scope.removeClass("blocks all")
 	.prependTo('.container').hide();
- scope.show().addClass(slide+"active active").siblings()
+ 	scope.show().addClass(slide+"active active").siblings()
  	.addClass(slide+'blocks blocks').removeClass("active");
- $('.container > div.basic').removeClass("basic");
+ 	$('.container > div.basic').removeClass("basic");
 });
-$(".container").on("click","[class*='blocks']",function(){
+	$(".container").on("click","[class*='blocks']",function(){
 	array=$(".active").attr("class").split(" ");
 	index=array.indexOf("active");
 	active=array[index-1];
 	fixactive=active.substr(0,active.length-6);
-	$(".active").addClass("allin "+fixactive+"blocks blocks").removeClass(fixactive+"active active");
+	$(".active").addClass("all "+fixactive+"blocks blocks").removeClass(fixactive+"active active");
 	scope=$(this);
 	scope.addClass(fixactive+"active active").removeClass(fixactive+"blocks blocks");
 	setTimeout(function(){scope.prependTo('.container');},50);
+});
+
+	$('.closse').on('click', function(){
+	$('.container > div').removeClass('active leftactive rightactive leftblocks rightblocks blocks');
+	setTimeout(function(){$('.container > div').addClass('basic all');},50);
+
 });
